962157aecb5281f1bc21a47822264119ef59458d
Sajib Saha <sajib.saha@brainstation-23.com>
2024-11-28 15:17:02 +0600

    Updated some code in main file

--- main.cpp
+++ main.cpp
@@ -12,16 +12,11 @@
 #include <nlohmann/json.hpp>
 #include <aws/lambda-runtime/runtime.h>
 
-using namespace std;
-using json = nlohmann::json;
 using namespace aws::lambda_runtime;
 
-string dummy_coupon = "782346120987";
-vector<pair<double, string>> stored_coupon;
-
 struct ResultConfig {
-    vector<pair<int, double>> lucky_configs;
-    vector<pair<int, double>> lucky_configs_RTL;
+    std::vector<std::pair<int, double>> lucky_configs;
+    std::vector<std::pair<int, double>> lucky_configs_RTL;
     double straight_config;
     double rumble_config;
 };
@@ -33,7 +28,7 @@ struct Request {
     bool is_paired;
 };
 
-int couponBitMask(string coupon) // Bitmasking Paired Coupon
+int couponBitMask(std::string coupon) // Bitmasking Paired Coupon
 {   
     int coupon_mask = 0;
 
@@ -45,7 +40,7 @@ int couponBitMask(string coupon) // Bitmasking Paired Coupon
     return coupon_mask;
 }
 
-double calculatePrizeForPaired(vector<int>& coupon_map, vector<pair<int, double>>& config, int lucky_coupon)
+double calculatePrizeForPaired(std::vector<int>& coupon_map, std::vector<std::pair<int, double>>& config, int lucky_coupon)
 {
     double total = 0;
 
@@ -68,7 +63,7 @@ double calculatePrizeForPaired(vector<int>& coupon_map, vector<pair<int, double>
     return total;
 }
 
-double calculatePrizeForNonPaired(Request request, ResultConfig& configs, map<string, int>& straight_coupon_map, map<string, int>& ramble_coupon_map, vector<string>& lucky_coupons, string g_coupon)
+double calculatePrizeForNonPaired(Request request, ResultConfig& configs, std::map<std::string, int>& straight_coupon_map, std::map<std::string, int>& ramble_coupon_map, std::vector<std::string>& lucky_coupons, std::string g_coupon)
 {
     double total = 0;
 
@@ -104,7 +99,7 @@ double calculatePrizeForNonPaired(Request request, ResultConfig& configs, map<st
             }
         }
 
-        total += max(LTR_prize, RTL_prize);
+        total += std::max(LTR_prize, RTL_prize);
     }
 
     sort(g_coupon.begin(), g_coupon.end());
@@ -113,7 +108,7 @@ double calculatePrizeForNonPaired(Request request, ResultConfig& configs, map<st
     return total;    
 }
 
-pair<string, double> generateCombinations(vector<string>& set, vector<int>& coupon_map, vector<pair<int, double>>& config, string g_coupon, int start, int k) {
+std::pair<std::string, double> generateCombinations(std::vector<std::string>& set, std::vector<int>& coupon_map, std::vector<std::pair<int, double>>& config, std::string g_coupon, int start, int k, std::vector<std::pair<double, std::string>>& stored_coupon) {
     if(g_coupon.length() == k) {
         int coupon_mask = couponBitMask(g_coupon);
         double prize = calculatePrizeForPaired(coupon_map, config, coupon_mask);
@@ -121,14 +116,16 @@ pair<string, double> generateCombinations(vector<string>& set, vector<int>& coup
         return {g_coupon, prize};
     }
 
-    pair<string, double> lucky_coupon = make_pair(dummy_coupon, 10000000.0); //Initial dummy coupon and prize
+    std::string dummy_coupon = "000000000000";
+
+    std::pair<std::string, double> lucky_coupon = make_pair(dummy_coupon, 10000000.0); //Initial dummy coupon and prize
 
     for(int i = start; i < set.size(); ++i) {
         if(i==start) {
-            lucky_coupon = generateCombinations(set, coupon_map, config, g_coupon + set[i], i + 1, k);
+            lucky_coupon = generateCombinations(set, coupon_map, config, g_coupon + set[i], i + 1, k, stored_coupon);
         }
         else {
-            pair<string, double> ps = generateCombinations(set, coupon_map, config, g_coupon + set[i], i + 1, k);
+            std::pair<std::string, double> ps = generateCombinations(set, coupon_map, config, g_coupon + set[i], i + 1, k, stored_coupon);
             if(ps.second < lucky_coupon.second) lucky_coupon = ps;
         }
     }
@@ -136,9 +133,9 @@ pair<string, double> generateCombinations(vector<string>& set, vector<int>& coup
     return lucky_coupon;
 }
 
-map<pair<int, char>, int> luckyCouponMapping(vector<string>& lucky_coupons)
+std::map<std::pair<int, char>, int> luckyCouponMapping(std::vector<std::string>& lucky_coupons)
 {
-    map<pair<int, char>, int> position_number_map;
+    std::map<std::pair<int, char>, int> position_number_map;
 
     for(int i=0; i<lucky_coupons.size(); ++i) {
         for(int j=0; j<lucky_coupons[i].length(); ++j) {
@@ -149,9 +146,9 @@ map<pair<int, char>, int> luckyCouponMapping(vector<string>& lucky_coupons)
     return position_number_map;
 }
 
-map<string, int> straightAndRambleMapping(vector<string>& coupons)
+std::map<std::string, int> straightAndRambleMapping(std::vector<std::string>& coupons)
 {
-    map<string, int> coupon_map;
+    std::map<std::string, int> coupon_map;
 
     for(int i=0; i<coupons.size(); ++i) {
         coupon_map[coupons[i]]++;
@@ -160,27 +157,12 @@ map<string, int> straightAndRambleMapping(vector<string>& coupons)
     return coupon_map;
 }
 
-string generateCouponNumber(Request request, map<pair<int, char>, int>& coupon_map, string coupon, bool is_random)
+std::string generateCouponNumber(Request request, std::map<std::pair<int, char>, int>& coupon_map, std::string coupon, bool is_random)
 {
     if(is_random) {
-        // Generate Random Positions
-        int number_of_random_positions = request.coupon_generate_length/1;
-
-        random_device dev;
-        mt19937 rng(dev());
-        uniform_int_distribution<mt19937::result_type> dist6(0, request.coupon_generate_length);
-
-        vector<int> random_positions;
-        for(int i=0; i<number_of_random_positions; ++i) {
-            random_positions.push_back(dist6(rng));
-        }
-
-        uniform_int_distribution<mt19937::result_type> dist7(request.coupon_generate_min_value, request.coupon_generate_max_value);
-        
-        for(int i=0; i<random_positions.size(); ++i) {
-            int pos = random_positions[i];
-            int rand_val = dist7(rng); // Generate Random Value For Random Position
-            coupon[pos] = '0'+rand_val;
+        for(int i=0; i<request.coupon_generate_length; ++i) {
+            int rand_val = rand() % (request.coupon_generate_max_value+1); // Generate Random Value For Random Position
+            coupon[i] = '0'+rand_val;
         }
     }
     else {
@@ -199,33 +181,33 @@ string generateCouponNumber(Request request, map<pair<int, char>, int>& coupon_m
     return coupon;
 }
 
-pair<string, double> pairedCoupon(vector<pair<int, double>>& config, int coupon_length, int coupon_generate_max_value, const json& input)
+std::pair<std::string, double> pairedCoupon(std::vector<std::pair<int, double>>& config, int coupon_length, int coupon_generate_max_value, const nlohmann::json& input, std::vector<std::pair<double, std::string>>& stored_coupon)
 {
     // Coupon Input
     int coupon_number = input["couponSize"];
-    vector<int> coupon_map;
+    std::vector<int> coupon_map;
 
     for(int i=0; i<coupon_number; ++i) {
-        string coupon = input["coupons"][i]["couponNumber"];
-        string coupon_type = input["coupons"][i]["type"];
+        std::string coupon = input["coupons"][i]["couponNumber"];
+        std::string coupon_type = input["coupons"][i]["type"];
         int quantity = input["coupons"][i]["quantity"];
 
         while(quantity--) coupon_map.push_back(couponBitMask(coupon));
     }
 
     // Create a set of available digits
-    vector<string> set;
+    std::vector<std::string> set;
     for(int i=1; i<=coupon_generate_max_value; ++i) {
-        string s="";
+        std::string s="";
         if(i<10) s+='0';
-        s += to_string(i);
+        s += std::to_string(i);
         set.push_back(s);
     }
 
-    pair<string, double> lucky_coupon = generateCombinations(set, coupon_map, config, "", 0, coupon_length);
+    std::pair<std::string, double> lucky_coupon = generateCombinations(set, coupon_map, config, "", 0, coupon_length, stored_coupon);
 
     // Sort the lucky coupon
-    vector<string> sortedCoupon;
+    std::vector<std::string> sortedCoupon;
     for(int i=0; i<coupon_length; i+=2) {
         sortedCoupon.push_back(lucky_coupon.first.substr(i, 2));
     }
@@ -238,18 +220,18 @@ pair<string, double> pairedCoupon(vector<pair<int, double>>& config, int coupon_
     return lucky_coupon;
 }
 
-pair<string, double> nonPairedCoupon(Request& request, ResultConfig& configs, const json& input)
+std::pair<std::string, double> nonPairedCoupon(Request& request, ResultConfig& configs, const nlohmann::json& input, std::vector<std::pair<double, std::string>>& stored_coupon)
 {
     int coupon_size = input["couponSize"];
-    vector<string> straight_coupons;
-    vector<string> lucky_coupons;
-    vector<string> ramble_coupons;
+    std::vector<std::string> straight_coupons;
+    std::vector<std::string> lucky_coupons;
+    std::vector<std::string> ramble_coupons;
 
     double total_selling_price = coupon_size * 0.5;
 
     for(int i=0; i<coupon_size; ++i) {
-        string coupon = input["coupons"][i]["couponNumber"];
-        string coupon_type = input["coupons"][i]["type"];
+        std::string coupon = input["coupons"][i]["couponNumber"];
+        std::string coupon_type = input["coupons"][i]["type"];
         int quantity = input["coupons"][i]["quantity"];
 
         if(coupon_type=="Straight") {
@@ -264,20 +246,20 @@ pair<string, double> nonPairedCoupon(Request& request, ResultConfig& configs, co
         }
     }
 
-    map<pair<int, char>, int> lucky_coupon_map = luckyCouponMapping(lucky_coupons);
-    map<string, int> straight_coupon_map = straightAndRambleMapping(straight_coupons);
-    map<string, int> ramble_coupon_map = straightAndRambleMapping(ramble_coupons);
-    map<string, int> stored_coupon_map;
+    std::map<std::pair<int, char>, int> lucky_coupon_map = luckyCouponMapping(lucky_coupons);
+    std::map<std::string, int> straight_coupon_map = straightAndRambleMapping(straight_coupons);
+    std::map<std::string, int> ramble_coupon_map = straightAndRambleMapping(ramble_coupons);
+    std::map<std::string, int> stored_coupon_map;
 
-    string initial_coupon = "";
+    std::string initial_coupon = "";
     for(int i=0; i<request.coupon_generate_length; ++i) initial_coupon += '0';
 
-    string generated_coupon_number = generateCouponNumber(request, lucky_coupon_map, initial_coupon, false);
+    std::string generated_coupon_number = generateCouponNumber(request, lucky_coupon_map, initial_coupon, false);
 
     int threshold = 100000;
     double prize_amount;
     double min_prize = 1000000000.0;
-    string min_prized_coupon;
+    std::string min_prized_coupon;
 
     while(true) {
         prize_amount = calculatePrizeForNonPaired(request, configs, straight_coupon_map, ramble_coupon_map, lucky_coupons, generated_coupon_number);
@@ -302,67 +284,79 @@ pair<string, double> nonPairedCoupon(Request& request, ResultConfig& configs, co
 
 invocation_response my_handler(invocation_request const& request)
 {
-    string payload = request.payload;
-    json input = json::parse(payload);
+    try
+    {
+        std::string payload = request.payload;
+        nlohmann::json input = nlohmann::json::parse(payload);
 
-    json output;
+        Request requests;
 
-    Request requests;
+        // Result Config Input
+        int result_config_size = input["configSize"];
 
-    // Result Config Input
-    int result_config_size = input["configSize"];
+        ResultConfig configs;
+        std::vector<std::pair<int, double>> lucky_configs;
+        std::vector<std::pair<int, double>> lucky_configs_RTL;
+        std::vector<std::pair<double, std::string>> stored_coupon;
 
-    ResultConfig configs;
-    vector<pair<int, double>> lucky_configs;
-    vector<pair<int, double>> lucky_configs_RTL;
+        for(int i=0; i<result_config_size; ++i) {
+            int lucky_match = input["configs"][i]["luckyMatch"];
+            double prize = input["configs"][i]["prize"];
+            std::string result_type = input["configs"][i]["resultType"];
+            bool left_to_right = input["configs"][i]["leftToRight"];
 
-    for(int i=0; i<result_config_size; ++i) {
-        int lucky_match = input["configs"][i]["luckyMatch"];
-        double prize = input["configs"][i]["prize"];
-        string result_type = input["configs"][i]["resultType"];
-        bool left_to_right = input["configs"][i]["leftToRight"];
-
-        if(result_type.find("Lucky") != string::npos) {
-            if(left_to_right) lucky_configs.push_back(make_pair(lucky_match, prize));
-            else lucky_configs_RTL.push_back(make_pair(lucky_match, prize));
+            if(result_type.find("Lucky") != std::string::npos) {
+                if(left_to_right) lucky_configs.push_back(std::make_pair(lucky_match, prize));
+                else lucky_configs_RTL.push_back(std::make_pair(lucky_match, prize));
+            }
+            else if(result_type=="Straight") configs.straight_config = prize;
+            else if(result_type=="Rumble") configs.rumble_config = prize;
         }
-        else if(result_type=="Straight") configs.straight_config = prize;
-        else if(result_type=="Rumble") configs.rumble_config = prize;
+        configs.lucky_configs = lucky_configs;
+        configs.lucky_configs_RTL = lucky_configs_RTL;
+
+        std::sort(configs.lucky_configs.rbegin(), configs.lucky_configs.rend());
+        std::sort(configs.lucky_configs_RTL.rbegin(), configs.lucky_configs_RTL.rend());
+
+        requests.coupon_generate_length = input["couponGenerateLength"];
+        requests.coupon_generate_max_value = input["couponGenerateMaxValue"];
+        requests.coupon_generate_min_value = input["couponGenerateMinValue"];
+        requests.is_paired = input["isPaired"];
+
+        if(requests.is_paired) {
+            std::pair<std::string, double> lucky_coupon = pairedCoupon(configs.lucky_configs, requests.coupon_generate_length, requests.coupon_generate_max_value, input, stored_coupon);
+        }
+        else {
+            std::pair<std::string, double> lucky_coupon = nonPairedCoupon(requests, configs, input, stored_coupon);
+        }
+        nlohmann::json suggestionArray = nlohmann::json::array();
+
+        std::sort(stored_coupon.begin(), stored_coupon.end(), [](const std::pair<double, std::string>& a, const std::pair<double, std::string>& b) {
+            if(a.first == b.first) {
+                return a.second > b.second;
+            }
+            return a.first < b.first;
+        });
+        
+        int numberOfResult = input["numberOfResult"];
+        int resultSize = stored_coupon.size();
+
+        for(int i=0; i<std::min(numberOfResult, resultSize); ++i) {
+            nlohmann::json obj;
+            obj["couponNumber"] = stored_coupon[i].second;
+            obj["prize"] = stored_coupon[i].first;
+            suggestionArray.push_back(obj);
+        }
+        nlohmann::json output;
+        
+        output["suggestions"] = suggestionArray;
+
+        return invocation_response::success(output.dump(), "application/json");
     }
-    configs.lucky_configs = lucky_configs;
-    configs.lucky_configs_RTL = lucky_configs_RTL;
-
-    sort(configs.lucky_configs.rbegin(), configs.lucky_configs.rend());
-    sort(configs.lucky_configs_RTL.rbegin(), configs.lucky_configs_RTL.rend());
-
-    requests.coupon_generate_length = input["couponGenerateLength"];
-    requests.coupon_generate_max_value = input["couponGenerateMaxValue"];
-    requests.coupon_generate_min_value = input["couponGenerateMinValue"];
-    requests.is_paired = input["isPaired"];
-
-    if(requests.is_paired) {
-        pair<string, double> lucky_coupon = pairedCoupon(configs.lucky_configs, requests.coupon_generate_length, requests.coupon_generate_max_value, input);
+    catch(const std::exception& e)
+    {
+        return invocation_response::failure(e.what(), "application/json");
     }
-    else {
-        pair<string, double> lucky_coupon = nonPairedCoupon(requests, configs, input);
-    }
-    json suggestionArray = json::array();
-
-    sort(stored_coupon.begin(), stored_coupon.end());
-    
-    int numberOfResult = input["numberOfResult"];
-    int resultSize = stored_coupon.size();
-
-    for(int i=0; i<min(numberOfResult, resultSize); ++i) {
-        json obj;
-        obj["couponNumber"] = stored_coupon[i].second;
-        obj["prize"] = stored_coupon[i].first;
-        suggestionArray.push_back(obj);
-    }
-
-    output["suggestions"] = suggestionArray;
-
-    return invocation_response::success(output.dump(), "application/json");
 }
 
 int main()
